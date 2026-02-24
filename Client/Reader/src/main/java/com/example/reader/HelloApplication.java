e3f4efc340b3b7526eefba123164abf55a4ad04b
dam xuan bac <damxuanbachochbt10@gmail.com>
2026-02-24 22:46:29 +0700

    Reader UI

--- Client/Reader/src/main/java/com/example/reader/HelloApplication.java
+++ Client/Reader/src/main/java/com/example/reader/HelloApplication.java
@@ -11,8 +11,11 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 1080, 630);
+        scene.getStylesheets().add(HelloApplication.class.getResource("reader.css").toExternalForm());
+        stage.setTitle("Reader Management");
+        stage.setMinWidth(980);
+        stage.setMinHeight(560);
         stage.setScene(scene);
         stage.show();
     }
