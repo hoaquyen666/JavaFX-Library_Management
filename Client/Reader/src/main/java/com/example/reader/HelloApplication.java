45f76046680c50891af608ac73e7b73de4e09487
dam xuan bac <damxuanbachochbt10@gmail.com>
2026-02-26 11:14:05 +0700

    Update CSS path and UI

--- Client/Reader/src/main/java/com/example/reader/HelloApplication.java
+++ Client/Reader/src/main/java/com/example/reader/HelloApplication.java
@@ -10,10 +10,16 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("reader-management-view.fxml"));
+
         Scene scene = new Scene(fxmlLoader.load(), 500, 450);
+
+        scene.getStylesheets().add(
+                HelloApplication.class.getResource("/com/example/reader/reader.css").toExternalForm()
+        );
+
         stage.setTitle("Thu Vien - Reader");
         stage.setScene(scene);
         stage.show();
-    }
-}
+
+        }}
