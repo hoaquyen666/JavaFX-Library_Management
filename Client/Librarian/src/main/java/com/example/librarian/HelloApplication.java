607a64375fe78f5258845556f185aafa88694e64
Thành An <thanhan1552006@gmail.com>
2026-02-28 17:40:52 +0700

    Không cho nười dùng kéo size màn hình

--- Client/Librarian/src/main/java/com/example/librarian/HelloApplication.java
+++ Client/Librarian/src/main/java/com/example/librarian/HelloApplication.java
@@ -13,6 +13,7 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("libra-main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 500);
         stage.setTitle("Hello!");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
