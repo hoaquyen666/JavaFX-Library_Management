ac11e04408bd18b60145f4c6f2baca96fe03f7ba
hoaquyen666 <hoaquyen666@gmail.com>
2026-02-28 22:06:52 +0700

    Add SeniorManager classes

---
+++ Client/SeniorManager/src/main/java/com/example/seniormanager/model/Account.java
@@ -0,0 +1,70 @@
+package com.example.seniormanager.model;
+
+public class Account {
+    private int accountId;
+    private String accountCode;
+    private Integer staffId;
+    private Integer readerId;
+    private String username;
+    private String passwordHash;
+    private String role;
+
+    public Account() {
+    }
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getAccountCode() {
+        return accountCode;
+    }
+
+    public void setAccountCode(String accountCode) {
+        this.accountCode = accountCode;
+    }
+
+    public Integer getStaffId() {
+        return staffId;
+    }
+
+    public void setStaffId(Integer staffId) {
+        this.staffId = staffId;
+    }
+
+    public Integer getReaderId() {
+        return readerId;
+    }
+
+    public void setReaderId(Integer readerId) {
+        this.readerId = readerId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
