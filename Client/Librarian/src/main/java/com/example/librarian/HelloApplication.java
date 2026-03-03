718b9504081df4ebcbf20b134e5ae66e6bb08ca5
Thành An <thanhan1552006@gmail.com>
2026-03-03 10:55:56 +0700

    CHo người dùng mở to giao diện

--- Client/Librarian/src/main/java/com/example/librarian/HelloApplication.java
+++ Client/Librarian/src/main/java/com/example/librarian/HelloApplication.java
@@ -12,7 +12,6 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/librarian/Library_Main_View/libra-main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 500);
-        stage.setResizable(false);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
