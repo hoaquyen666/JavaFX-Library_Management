9cbf213f655ea1319b62e2f851dd640a93cd8d00
Thành An <thanhan1552006@gmail.com>
2026-03-04 18:57:01 +0700

    Thêm giao diện quản lý nhân viên

--- Client/Administrator/src/main/java/com/example/administrator/HelloApplication.java
+++ Client/Administrator/src/main/java/com/example/administrator/HelloApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/administrator/staff-management/main-view/staff-management-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 800);
+        stage.setTitle("Quản lý nhân viên");
         stage.setScene(scene);
         stage.show();
     }
