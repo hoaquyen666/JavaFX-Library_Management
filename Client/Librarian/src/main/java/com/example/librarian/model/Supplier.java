26e42d2f011018c426188871a4fde9d21fa8ac20
Đồ Tể Campuchia <dinhnguyenhoang946@gmail.com>
2026-03-14 22:22:37 +0700

    them controller,DAO,fxml,va smth cho supplier management

---
+++ Client/Librarian/src/main/java/com/example/librarian/model/Supplier.java
@@ -0,0 +1,82 @@
+package com.example.librarian.model;
+
+public class Supplier {
+
+    private int id;
+    private String code;
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+    private String status;
+
+    public Supplier() {
+    }
+
+    public Supplier(int id, String code, String name, String email,
+                    String phone, String address, String status) {
+        this.id = id;
+        this.code = code;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
